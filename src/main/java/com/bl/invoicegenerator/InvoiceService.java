935f978a13cd6118d4abb201bb697095d6e40306
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-13 15:47:08 +0530

    ADDED:Test Case For Total Fair

--- src/main/java/com/bl/invoicegenerator/InvoiceService.java
+++ src/main/java/com/bl/invoicegenerator/InvoiceService.java
@@ -1,7 +1,3 @@
 package com.bl.invoicegenerator;
 public class InvoiceService {
-    public static void main(String args[])
-    {
-        System.out.println("Welcome Message");
-    }
 }
---
+++ src/test/java/com/bl/cabservice/InvoiceServiceTest.java
@@ -0,0 +1,21 @@
+package com.bl.cabservice;
+import com.bl.invoicegenerator.InvoiceService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class InvoiceServiceTest {
+    InvoiceService invoiceService;
+
+    @Before
+    public void setUp() {
+        invoiceService = new InvoiceService();
+    }
+
+    @Test
+    public void givenDistanceAndTime_shouldReturnTotalFare() {
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceService.calculateFare(distance, time);
+        Assert.assertEquals(25, fare, 0.0);
+    }
+}
