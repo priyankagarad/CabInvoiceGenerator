423519ea2e604742930eaac7628580648d4948f6
priyankagarad <garadpriyanka1995@gmail.com>
2020-05-13 16:11:11 +0530

    ADDED:Test Case For Calculate Total Fair For Multiple Ride

--- src/test/java/com/bl/cabservice/InvoiceServiceTest.java
+++ src/test/java/com/bl/cabservice/InvoiceServiceTest.java
@@ -17,4 +17,11 @@ public class InvoiceServiceTest {
         double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    public void givenMultipleRides_whenRide_ShouldReturnTotalFare() {
+        Ride[] rides = {new Ride(2.0,5), new Ride(0.1,1)};
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30,fare,0.0);
+    }
 }
